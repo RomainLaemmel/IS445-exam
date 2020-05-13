1e77c153fab968da18ffc23fb2827439f4306594
Romain <romainlaemmel@gmail.com>
2020-05-13 05:49:33 +0200

    Step 3: Find all even numbers

--- main.js
+++ main.js
@@ -3,6 +3,25 @@ var input1 = document.getElementById('number1');
 var input2 = document.getElementById('number2');
 var result = document.getElementById('result');
 
+function findAllEvenNumbers(nb1, nb2) {
+    var evenNumbers = [];
+
+    if (nb1 > nb2) {
+        var tmp;
+        tmp = nb1;
+        nb1 = nb2;
+        nb2 = tmp;
+    }
+
+    for (var i = nb1; i <= nb2; i = i + 1) {
+        if (i % 2 === 0) {
+            evenNumbers.push(i);
+        }
+    }
+
+    return evenNumbers;
+}
+
 const handler = function (e) {
 
     e.preventDefault();
@@ -25,6 +44,14 @@ const handler = function (e) {
     } else {
         if (nb2 < 2 || nb2 > 100) {
             res = res + '<span class="error-line">Number 2 input ' + input2.value + ' is not in the range of 2 and 100</span>';
+        }
+    }
+
+    if (res === '') {
+        var evenNumbers = findAllEvenNumbers(nb1, nb2);
+        var res = '<span class="text-info result-line">There are ' + evenNumbers.length + ' even numbers:</span>';
+        if (evenNumbers.length > 0) {
+            res = res + '<span class="text-info result-line">' + evenNumbers + '</span>';
         }
     }
 
